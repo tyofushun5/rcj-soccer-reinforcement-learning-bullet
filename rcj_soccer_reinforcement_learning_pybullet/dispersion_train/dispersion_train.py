7ef8e7696105258ac040bde132d4873d37c559a1
shun <zenshun10@gmail.com>
2025-09-05 00:51:57 +0900

    9/5 commit

--- rcj_soccer_reinforcement_learning_pybullet/dispersion_train/dispersion_train.py
+++ rcj_soccer_reinforcement_learning_pybullet/dispersion_train/dispersion_train.py
@@ -1,6 +1,6 @@
 import os
 
-from stable_baselines3 import PPO
+from sb3_contrib import RecurrentPPO
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from rcj_soccer_reinforcement_learning_pybullet.environment.environment import Environment
 from stable_baselines3.common.callbacks import CheckpointCallback
@@ -12,13 +12,13 @@ os.makedirs(save_dir, exist_ok=True)
 
 checkpoint_callback = CheckpointCallback(save_freq=1000000,
                                          save_path=save_dir,
-                                         name_prefix='default_model_v1',
+                                         name_prefix='default_model_v6',
                                          save_replay_buffer=True,
                                          save_vecnormalize=True)
 
 def make_env():
     def _init():
-        env = Environment(max_steps=20480,
+        env = Environment(max_steps=10000,
                           create_position=[4.0, 0.0, 0.0],
                           magnitude=21.0,
                           gui=False)
@@ -28,31 +28,37 @@ def make_env():
 
 def main():
 
-    num_envs = 25
+    num_envs = 12
     env = SubprocVecEnv([make_env() for _ in range(num_envs)])
 
     policy_kwargs = {
-        "net_arch": dict(pi=[256, 256, 256, 256],
-                         vf=[256, 256, 256, 256]),
+        "net_arch": dict(pi=[128, 128, 128, 128],
+                         vf=[128, 128, 128, 128]),
+        "lstm_hidden_size": 256,
+        "n_lstm_layers": 1,
+        "shared_lstm": False,
+        "enable_critic_lstm": True
     }
 
-    model = PPO('MlpPolicy',
+
+    model = RecurrentPPO('MlpLstmPolicy',
                          env,
                          device='cuda',
                          verbose=1,
-                         n_epochs=10,
-                         n_steps=4096,
-                         batch_size=4096*num_envs,
+                         n_epochs=1,
+                         n_steps=128,
+                         batch_size=128*num_envs,
                          gamma=0.99,
-                         policy_kwargs=policy_kwargs
+                         policy_kwargs=policy_kwargs,
+                         max_grad_norm=1.0
                          )
 
     model.learn(total_timesteps=100000000,
-                # callback=checkpoint_callback,
+                callback=checkpoint_callback,
                 progress_bar=True
                 )
 
-    model.save(os.path.join(save_dir, 'default_model_v7'))
+    model.save(os.path.join(save_dir, 'default_model_v1'))
 
     env.close()
 
